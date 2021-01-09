1233cb2480e22d2a5a3ec9ce8bc7f4ac8517932e
Aaron <hanqtj@163.com>
2021-01-09 16:13:59 +0800

    获取ApplicationContext

--- src/main/java/com/xinwei/spring/MessageServiceImpl.java
+++ src/main/java/com/xinwei/spring/MessageServiceImpl.java
@@ -1,6 +1,6 @@
 package com.xinwei.spring;
 
-public class MessageServiceImpl {
+public class MessageServiceImpl implements MessageService {
     public String getMessage() {
         return "hello world";
     }
